da943a6d894421097672cdfab59e3d0989f61adb
Kateryna <k.kolobaieva-2022@kpi.ua>
2019-12-02 11:34:59 +0200

    Update models.py

--- 1/models.py
+++ 1/models.py
@@ -12,4 +12,29 @@ session.execute("SELECT temperature, seasonality from keyspace1.event_clothers w
 session.execute("SELECT user_name, style,outwear,lowerwear,shoes from keyspace1.clothers_options where style_id=1;")
 session.execute("SELECT count_of_version from keyspace1.clothers_options where  options_id=1;")
 
+class option_clothes():
+    def __init__(self, option_id, clothe_id):
+        self.option_id = option_id
+        self.clothe_id = clothe_id
+
+### DELETE ###
+delete_style_name = session.prepare("DELETE FROM clothes WHERE = ?")
+session.execute(delete_style_name, ['romantic'])
+session.execute(delete_style_name, ['gala'])
+session.execute(delete_style_name, ['office'])
+
+delete_ = session.prepare("DELETE FROM clothes WHERE = ?")
+session.execute(delete_outwear, ['coat'])
+session.execute(delete_outwear, ['dress'])
+session.execute(delete_outwear, ['t-shirt'])
+
+delete_option_id = session.prepare("DELETE FROM options WHERE = ?")
+session.execute(delete_option_id, ['1'])
+session.execute(delete_option_id, ['2'])
+session.execute(delete_option_id, ['3'])
+
+delete_clothe_id = session.prepare("DELETE FROM clothe WHERE = ?")
+session.execute(delete_clothe_id, ['1'])
+session.execute(delete_clothe_id, ['2'])
+session.execute(delete_clothe_id, ['3'])
 
